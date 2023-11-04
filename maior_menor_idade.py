582943ac476909afcb2a4bff1a45e1a277511834
janeilson-araujo <146784212+janeilson-araujo@users.noreply.github.com>
2023-11-04 10:08:45 -0300

    Add files via upload

---
+++ maior_menor_idade.py
@@ -0,0 +1,23 @@
+# Exercício Python 28 : Crie um programa que leia o ano de nascimento de sete pessoas. No final,
+# mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
+import datetime
+
+maior_idade = []
+menor_idade = []
+
+hoje = datetime.datetime.now()
+year = hoje.year
+
+print("digite uma lista de anos de nascimento e saiba quais são maiores de idade")
+
+for i in range(1,8):
+    ano = int(input(f"digite o {i}° ano de nascimento:"))
+    idade = year - ano
+    if idade >= 18 :
+        maior_idade.append(ano)
+    else :
+        menor_idade.append(ano)
+
+print(f'''os maiores de idade são:{maior_idade}
+os menores de idade são:{menor_idade}''')        
